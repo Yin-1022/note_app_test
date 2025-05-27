7d06aa818a33f8b42b44414bc0cc479f45cbb8ed
1234 <s111216032@stu.ntue.edu.tw>
2025-05-27 17:35:05 +0800

    add my name

--- lib/model/note.dart
+++ lib/model/note.dart
@@ -2,10 +2,12 @@ class Note {
   final String title;
   final String description;
   final DateTime dateTime;
+  final String Ming;
 
   Note({
     required this.title,
     required this.dateTime,
     required this.description,
+    this.Ming = ""
   });
 }
