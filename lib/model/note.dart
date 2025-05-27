b88c7b94fb2d9a5d67e3513a7ee664695f261613
Yin-1022 <kv.huang931022@gmail.com>
2025-05-27 17:33:28 +0800

    name

--- lib/model/note.dart
+++ lib/model/note.dart
@@ -2,10 +2,12 @@ class Note {
   final String title;
   final String description;
   final DateTime dateTime;
+  final String name;
 
   Note({
     required this.title,
     required this.dateTime,
     required this.description,
+    required this.name,
   });
 }
